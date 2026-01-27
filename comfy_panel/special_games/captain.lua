c66d88b15d70fb7806e54dd36fa9d83c72f768f6
cogito123 <59515242+cogito123@users.noreply.github.com>
2026-01-27 19:12:04 +0100

    captain: rename pick list entry update function
    Replaces 'update' with 'destroy' since it's more accurate.

--- comfy_panel/special_games/captain.lua
+++ comfy_panel/special_games/captain.lua
@@ -432,7 +432,7 @@ local function auto_pick_all_of_group(playerName)
                 switch_team_of_player(playerName, f)
                 player.print({ 'captain.comms_reminder' }, { color = Color.cyan })
                 table_remove_element(special.listPlayers, playerName)
-                CaptainUI.try_update_picking_ui_list_for_each(special.captainList, playerName)
+                CaptainUI.try_destroy_picking_ui_list_entry_for_each(special.captainList, playerName)
                 special.captain_pick_timer[f] = special.captain_pick_timer[f] + special.captain_pick_timer_gain
             end
         end
@@ -1538,7 +1538,7 @@ local function assign_player(player)
     end
 
     special.captain_pick_timer[f] = special.captain_pick_timer[f] + special.captain_pick_timer_gain
-    CaptainUI.try_update_picking_ui_list_for_each(special.captainList, player)
+    CaptainUI.try_destroy_picking_ui_list_entry_for_each(special.captainList, player)
 
     if is_player_in_group_system(player) then
         auto_pick_all_of_group(player)
